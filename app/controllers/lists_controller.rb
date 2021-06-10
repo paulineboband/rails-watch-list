bd48ac44afaa65b72ba58c967d820564e157dd96
Pauline Boband <paulineboband@gmail.com>
2021-06-10 14:44:54 +0200

    routes, controllers, views

---
+++ app/controllers/lists_controller.rb
@@ -0,0 +1,27 @@
+class ListsController < ApplicationController
+  def index
+    @lists = List.all
+  end
+
+  def show
+    @list = List.find(params[:id])
+  end
+
+  def new
+    @list = List.new
+  end
+
+  def create
+    @list = List.new(list_params)
+    @list.save
+
+    # no need for app/views/restaurants/create.html.erb
+    redirect_to list_path(@list)
+  end
+
+  private
+
+  def list_params
+    params.require(:list).permit(:name)
+  end
+end
